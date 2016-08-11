8d24d55b9617cd582f24d00c938fea1a76eb07aa
SwiftiCode <swifticode@203.203-211-151.static.qala.com.sg>
2016-08-11 19:40:58 +0800

    Table view complete

---
+++ Intro18/Book.swift
@@ -0,0 +1,16 @@
+//
+//  Book.swift
+//  Intro18
+//
+//  Created by SwiftiCode on 11/8/16.
+//  Copyright © 2016 SwiftiCode. All rights reserved.
+//
+
+import Foundation
+
+struct Book {
+    
+    var bookTitle: String
+    var bookSummary: String
+    
+}
---
+++ Intro18/BookListTableViewController.swift
@@ -0,0 +1,115 @@
+//
+//  BookListTableViewController.swift
+//  Intro18
+//
+//  Created by SwiftiCode on 11/8/16.
+//  Copyright © 2016 SwiftiCode. All rights reserved.
+//
+
+import UIKit
+
+class BookListTableViewController: UITableViewController {
+    
+    // MARK: Properties
+    var bookList = [Book]()
+    
+    // MARK: Helper function to load sample book
+    func loadSampleBook()  {
+        
+        let book1 = Book(bookTitle: "Book Title 1", bookSummary: "Book 1 Summary")
+        let book2 = Book(bookTitle: "Book Title 2", bookSummary: "Book 2 Summary")
+        let book3 = Book(bookTitle: "Book Title 3", bookSummary: "Book 3 Summary")
+        
+        bookList += [book1, book2, book3]
+        
+    }
+    
+    // MARK: Template
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
+        loadSampleBook()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return bookList.count
+    }
+
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("BookListCell", forIndexPath: indexPath)
+
+        // Configure the cell...
+        let myBook = bookList[indexPath.row]
+        
+        cell.textLabel?.text = myBook.bookTitle
+
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
